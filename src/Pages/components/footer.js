92b3a735ca759beffbdc5d7471de35dd3b59356f
Gordon <seednaruto123@gmail.com>
2022-07-29 01:39:35 +0800

    20220729
    finish fix and Cssing Footer finally.
    so footer be component, i can use all my page.

--- src/Pages/components/footer.js
+++ src/Pages/components/footer.js
@@ -7,25 +7,31 @@ function footer () {
   return (
     <>
       <div id="Footer">
-        <div className="FootContainer">
+        <div className="FootContainer mt-4">
           <div className="FootInfo">
-            <div className="Logo">
+            <div className="FootLogo">
               <img src={LOGO} alt="" />
             </div>
-            <div>
-              <p>新北市三重區環河南路999號99樓9號</p>
-              <span>086-272-2670</span>
-              <br />
-              <p>3月20日 ～ 9月30日 - 10:30 ～ 18:00</p>
-              <p>10月01日 ～ 3月19日 - 10:30 ～ 17:00</p>
-              <p>gordon_chaxiures@gmail.com</p>
+            <div className="FootContent ">
+              <p className="FcontentTitle">新北市三重區環河南路999號99樓9號</p>
+              <p className="FPhone">086-272-2670</p>
+              <p className="Fdate">3月20日 ～ 9月30日 - 10:30 ～ 18:00</p>
+              <p className="Fdate">10月01日 ～ 3月19日 - 10:30 ～ 17:00</p>
+              <p className="FEmail">gordon_chaxiures@gmail.com</p>
             </div>
           </div>
-          <div className="Emailbox">
-            <input type="text" name="email" />
-            <button className="emailBtn"></button>
+          <div className="Emailbox mt-3">
+            <div className="EboxCenter">
+              <input
+                type="text"
+                name="email"
+                className="Emailinput"
+                placeholder="請輸入您的email訂閱我們"
+              />
+              <button className="emailBtn"></button>
+            </div>
           </div>
-          <p>
+          <p className="copyright">
             Copyright and designed by Gordon and Shao food restaurant SDN BHD
           </p>
         </div>
